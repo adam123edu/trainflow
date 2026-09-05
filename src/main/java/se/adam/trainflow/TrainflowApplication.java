47d775cb57e8d44bb1017a11ca3acc30d9bd0507
Adam <chartoi17@outlook.com>
2026-09-05 11:55:25 +0200

    Initial project setup: Spring Boot 4, JPA, H2, validation, Swagger

---
+++ src/main/java/se/adam/trainflow/TrainflowApplication.java
@@ -0,0 +1,13 @@
+package se.adam.trainflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/adam/trainflow/TrainflowApplicationTests.java
@@ -0,0 +1,13 @@
+package se.adam.trainflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
